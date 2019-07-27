9a8d3999ab76230636344e709d7c8775ecf35a01
Pedro Cruz <pedrohlcruz@gmail.com>
2019-07-27 18:02:31 -0300

    modified:   playlistURL.py

--- playlistURL.py
+++ playlistURL.py
@@ -5,46 +5,51 @@ from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.common.by import By
 
 from termcolor import colored
+import subprocess
 import time
 import os
 import sys 
 
 mainUrl = ''
+artist = ''
 songsUrl = [None]
 elements = [None]
-names = [None]
+names = [None] 
 songsUrl = []
 elements = []
 names = []
-number_of_songs = 0
+
 
 driverPath = '/Applications/chromedriver'
 
+# Web driver settings
+# options = Options()
+# options.add_argument('--headless')
+# driver = webdriver.Chrome(options=options,executable_path=driverPath)
+driver = webdriver.Chrome(executable_path=driverPath)
+wait = WebDriverWait(driver,100000)
+
 def user():
 
-    global mainUrl
-    global number_of_songs
+    global mainUrl 
+    global artist
 
     os.system('clear')
     mainUrl = input('•The url for the first song in the playlist->')
+    artist = input('•The artist->')
     
 
 def gatherLink():
 
     global mainUrl
-    global number_of_songs
     global songsUrl
     global elements
     global names
+    global driver
+    
 
     generalXpath = '//*[@id="wc-endpoint"]'
     
-    # Web driver settings
-    options = Options()
-    options.add_argument('--headless')
-    driver = webdriver.Chrome(options=options,executable_path=driverPath)
-    wait = WebDriverWait(driver,100000)
-
     # Go to the main url and wait until the element appears
     driver.get(mainUrl)
     firstItem = wait.until(ec.visibility_of_element_located((By.XPATH, generalXpath)))
@@ -54,10 +59,8 @@ def gatherLink():
 
     for element in elements:
 
-        print(element.get_attribute("href"))
-        
         # Clear the name
-        name = element.text.replace('TV SIZE','').replace('\n','').replace('IwannalickMenatsFeet', '')
+        name = element.text.replace('TV SIZE','').replace('\n','').replace('IwannalickMenatsFeet','')
         for letter in name:
 
             if letter in '1234567890:▶':
@@ -69,15 +72,94 @@ def gatherLink():
         if 'blocked' in name:
             #If it is block, skip and go to the next itereation 
             continue
+
         else:
+
+            # Clean the rawUrl-> so the url is just the video's, and not the playlist
+            rawUrl = element.get_attribute("href")
+            rawUrl = rawUrl.split('&')
+            url = rawUrl[0]
+            
             # Add the titles to the names list
             names.append(name)
             # Add the links to the list 
-            songsUrl.append(element.get_attribute("href"))
+            songsUrl.append(url)
         
 
 def download():
-    youtUrl = ''
+
+    global driver 
+    global names 
+    global songsUrl
+    global artist
+
+    # Change to the download directory
+    os.chdir('/Users/pedrocruz/Downloads')
+
+    youtUrl = 'https://yout.com/'
+    inputXpath = '/html/body/div[1]/div/div/div/div[1]/form/div/input'
+    nameXpath = '/html/body/div[2]/div/div[1]/div/div[2]/div[3]/div[3]/div[3]/div/input'
+    artistXpath = '/html/body/div[2]/div/div[1]/div/div[2]/div[3]/div[3]/div[4]/div/input'
+    recordXpath = '/html/body/div[2]/div/div[1]/div/div[2]/div[3]/div[3]/div[5]/button'
+    Download = False
+
+    # Go to that website 
+    driver.get(youtUrl)
+    # Wait until the input box loads
+    inputBox = wait.until(ec.visibility_of_element_located((By.XPATH, inputXpath)))
+
+    # Goes through all the links and type the url in the input box
+    for url,name in zip(songsUrl,names):
+        
+        # Types the link in the input box
+        driver.find_element_by_xpath(inputXpath).send_keys(url)
+        
+        # Wait until the name box loads
+        nameBox = wait.until(ec.visibility_of_element_located((By.XPATH, nameXpath)))
+
+        # Clear the input box and Send the title of the song to the name box
+        driver.find_element_by_xpath(nameXpath).clear()
+        driver.find_element_by_xpath(nameXpath).send_keys(name)
+
+        # Clear the input box and send the artist's name to the artist's input box
+        driver.find_element_by_xpath(artistXpath).clear()
+        driver.find_element_by_xpath(artistXpath).send_keys(artist)
+
+        # Press the record button
+        driver.find_element_by_xpath(recordXpath).click() 
+        
+        # Goes in loop to see if the file has been downloaded
+        # Get the initial files in the Downloads directory, to compare it to future iterations
+        lastLs = subprocess.check_output('ls')
+        lastLs = lastLs.split()
+        
+        ls = []
+
+        while Download == False:
+            
+            # Check again, if it is different from the first one, it means the file was added
+            ls = subprocess.check_output('ls')
+            ls = ls.split()
+
+            if ls != lastLs:
+                Download = True
+            
+
+            # for output in ls:
+
+            #     print(name.replace(' ','').replace(',','').replace(' ','-')+'.mp3')
+            #     if output == name.replace(',','').replace(' ','-')+'.mp3':
+            #         Download = True
+            #     else:
+            #         pass
+
+        # Update variables for next iteration
+        Download = False
+        ls = []
+
+        
+
+
     
     
 
@@ -85,3 +167,4 @@ def download():
 if __name__=='__main__':
     user()
     gatherLink()
+    download()
