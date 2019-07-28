610c7e31b5b225dcd15b684d4cc208f556a032c3
Pedro Cruz <pedrohlcruz@gmail.com>
2019-07-28 10:06:17 -0300

    Update playlistURL.py

--- playlistURL.py
+++ playlistURL.py
@@ -4,7 +4,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.common.by import By
 
-from termcolor import colored
+
 import subprocess
 import time
 import os
